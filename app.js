520c7d8284a3b3d55a774bd60da9a759d1617550
Christine Cion <cion.c.bscs@gmail.com>
2023-09-16 11:22:23 +0800

    create models and schema/getting and saving data

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// import model
+const Item = require('./models/items');
 const app = express();
 const mongodb = 'mongodb+srv://admin:admin@cluster0.1blzevm.mongodb.net/itemDB?retryWrites=true&w=majority';
 mongoose.connect(mongodb, { useNewUrlParser: true, 
@@ -10,7 +12,29 @@ useUnifiedTopology: true}).then(() => {
 
 app.set('view engine','ejs');
 
+// create items once refresh
+app.get('/create-item',(req, res) => {
+    const item = new Item({
+        name: 'computer',
+        price: 35000
+    });
+    item.save().then(result=>res.send(result)).catch(err =>
+        console.log(err))
+})
+// get all items
+app.get('/get-items',(req, res) => {
 
+    Item.find().then(result=>res.send(result)).catch(err =>
+        console.log(err))
+
+})
+// get item by id
+app.get('/get-item',(req, res) => {
+
+    Item.findById('65051d594ab5a4e60e307308').then(result=>res.send(result)).catch(err =>
+        console.log(err))
+
+})
 app.get('/', (req, res) => {
     const items = [
         { name: 'smart phone', price: 15000},
