4152ae467c52a8d09d55da186a7eb0a98d82fd16
Christine Cion <cion.c.bscs@gmail.com>
2023-09-16 09:21:28 +0800

    generate dynamic content with ejs

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ app.set('view engine','ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const items = [
+        { name: 'smart phone', price: 15000},
+        { name: 'book', price: 150},
+        { name: 'computer', price: 35000},    
+    ]
+    res.render('index', { items });
 })
 app.get('/add-item', (req, res)=> {
     res.render('add-item');
